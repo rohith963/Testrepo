2e6225d6c4545fe5f873aff226a860da0d51cc28
rohith <rohithcherry741@gmail.com>
2025-05-14 15:41:14 +0530

    commited

---
+++ ElectricityBillSystem/src/com/rohith/BillSystem/BillCalculator.java
@@ -0,0 +1,22 @@
+package com.rohith.BillSystem;
+
+public class BillCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int units = 280;
+		double billToPay = 0;
+		if(units<100) {
+			billToPay = units * 1.20;
+		}
+		else if(units < 300) {
+			billToPay = 100 * 1.20 + (units - 100)*2;
+		}
+		System.out.print("The electricity bill for " +units+" is : "+billToPay);
+
+	}
+
+
+	}
+
+
